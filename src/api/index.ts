4157ce955475d0c98e68853a2dd21e4210c57740
Clinton Andrews <clint@pollyspeople.net>
2021-04-26 11:31:17 -0400

    add newuser endpoint

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,11 @@
-import * as User from './endpoints/user';
 import * as Fueling from './endpoints/fueling';
+import * as NewUser from './endpoints/new-user';
 import * as Plan from './endpoints/plan';
+import * as User from './endpoints/user';
 
 export const Api = {
     Fueling,
+    NewUser,
     Plan,
     User,
 };
