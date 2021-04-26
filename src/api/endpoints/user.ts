b26460f1a98c8a17bfdd104ce473caf42e3c2516
Clinton Andrews <clint@pollyspeople.net>
2021-04-26 11:31:58 -0400

    update get user for current user

--- src/api/endpoints/user.ts
+++ src/api/endpoints/user.ts
@@ -1,7 +1,10 @@
 import { apiBase } from '../api-base';
 
-export const getUser = () => 
-    apiBase.client.get<User>(`user`);
+export const getUser = () =>
+    apiBase.client.get<CurrentUser>(`user`);
+
+export const getUserExists = () =>
+    apiBase.client.get<boolean>(`user/exists`);
 
 export const getNewUser = () =>
     apiBase.client.get<User>(`newuser`);
