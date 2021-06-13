695f01e9e8f92da1076a71c69a98cc776d550908
Clinton Andrews <clint@pollyspeople.net>
2021-06-13 00:51:19 -0400

    update map and worker

--- src/register-background-worker.ts
+++ src/register-background-worker.ts
@@ -8,7 +8,7 @@ export const registerBackgroundWorker = () => {
                 // The service worker will not handle requests until this page and any
                 // other instances of this page (in other tabs, etc.) have been
                 // closed/reloaded.
-                console.info('succeeded', registration);
+                // console.info('succeeded', registration);
             }).catch(error => {
                 // Something went wrong during registration. The service-worker.js file
                 // might be unavailable or contain a syntax error.
