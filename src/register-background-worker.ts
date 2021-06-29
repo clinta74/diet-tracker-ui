cf03cbb519e03a1fa074d0e6c1bbd5167dffa91b
Clinton Andrews <clint@pollyspeople.net>
2021-06-28 23:50:43 -0400

    update manifest

--- src/register-background-worker.ts
+++ src/register-background-worker.ts
@@ -2,17 +2,13 @@ export const registerBackgroundWorker = () => {
     if ('serviceWorker' in navigator) {
         // Override the default scope of '/' with './', so that the registration applies
         // to the current directory and everything underneath it.
-        navigator.serviceWorker.register('/service-worker.js', { scope: './' })
+        navigator.serviceWorker.register('/service-worker.js', { scope: '/' })
             .then(() => {
-                // At this point, registration has taken place.
-                // The service worker will not handle requests until this page and any
-                // other instances of this page (in other tabs, etc.) have been
-                // closed/reloaded.
-                // console.info('succeeded', registration);
+                console.log('Service Worker registered successfully.');
             }).catch(error => {
                 // Something went wrong during registration. The service-worker.js file
                 // might be unavailable or contain a syntax error.
-                console.log('error', error);
+                console.log('Service Worker registration failed:', error);
             });
     } else {
         // The current browser doesn't support service workers.
--- src/static/service-worker.js
+++ src/static/service-worker.js
@@ -1,1 +1,14 @@
 // This service worker doesn't actually do anything!
+self.addEventListener('install', e => {
+    e.waitUntil(
+        // after the service worker is installed,
+        // open a new cache
+        caches.open('my-pwa-cache').then(cache => {
+            // add all URLs of resources we want to cache
+            return cache.addAll([
+                '/',
+                '/index.html',
+            ]);
+        })
+    );
+});
