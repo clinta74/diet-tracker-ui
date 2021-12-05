f22f02a185dcb16eea5093a1e2e11c5277254e71
Clinton Andrews <clinta74@live.com>
2021-12-04 21:48:43 -0500

    move config

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 export const CONFIG = {
-    API_URL: process.env.API_URL || 'https://localhost:5001'
+    API_URL: process.env.API_URL,
 }
 
 export const AUTH0 = {
-    domain: process.env.REACT_APP_AUTH0_DOMAIN || 'dev-clinta74.us.auth0.com',
+    domain: process.env.REACT_APP_AUTH0_DOMAIN,
     scope: 'openid token email',
-    audience: process.env.REACT_APP_AUTH0_AUDIENCE || 'https://diet-tracker.pollyspeople.net',
-    clientId: process.env.REACT_APP_AUTH0_CLIENT_ID || 'I1EPbV0JzhQ4sMebkA5XSg0RhYhJBa1k',
+    audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+    clientId: process.env.REACT_APP_AUTH0_CLIENT_ID,
 }
