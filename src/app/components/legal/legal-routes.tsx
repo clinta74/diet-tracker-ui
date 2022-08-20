c9662ba0aeeaf263c5c10ea1c12734e32b82c2e3
Clinton Andrews <clinta74@live.com>
2022-08-20 19:18:01 -0400

    add terms route

--- src/app/components/legal/legal-routes.tsx
+++ src/app/components/legal/legal-routes.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { PrivacyPolicy } from './privacy-policy';
+import { TermsConditions } from './terms-and-conditions';
 
 export const LegalRoutes: React.FC = () => {
     return (
         <Routes>
             <Route path="privacy" element={< PrivacyPolicy />} />
+            <Route path="terms" element={< TermsConditions />} />
         </Routes>
     );
 }
